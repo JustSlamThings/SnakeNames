27837cb83f645cbf3bdcf4f37943894b5bb8954f
JustSlamThings <justslamthings@gmail.com>
2022-12-11 12:07:29 -0500

    Snake names are cools

---
+++ index.js
@@ -0,0 +1,24 @@
+// const snakeNames = require('snake-names')
+
+// console.log(snakeNames.badassRandom())
+
+
+// const snakeNames = require('snake-names')
+
+// let namePicked = snakeNames.random()
+
+// console.log('You should name this snake:', namePicked)
+// let femaleSnakeNames = snakeNames.female
+// let randomIndex = Math.floor(Math.random() * femaleSnakeNames.length)
+// console.log("A great female snake name is:", femaleSnakeNames[randomIndex])
+const snakeNames = require('snake-names')
+                
+let namePicked = snakeNames.random()
+                
+console.log('You should name this snake:', namePicked)
+                
+let femaleSnakeNames = snakeNames.female
+                
+let randomIndex = Math.floor(Math.random() * femaleSnakeNames.length)
+                
+console.log("A great female snake name is:", femaleSnakeNames[randomIndex])
